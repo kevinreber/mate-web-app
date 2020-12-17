b19bf85d88990e146c9ac49745c8809dc379b7d2
kevinreber <kevinreber1@gmail.com>
2020-12-16 19:56:37 -0800

    modified Course List for new design layout

--- src/pages/Courses/components/CourseList/CourseList.jsx
+++ src/pages/Courses/components/CourseList/CourseList.jsx
@@ -4,32 +4,46 @@ import { PropTypes } from 'prop-types';
 
 /** Components & Helpers */
 import CourseCard from '../CourseCard/CourseCard';
+import NoData from '../../../../components/NoData/NoData';
+import capitalizeFirstLetter from '../../../../utils/capitalizeFirstLetter';
 
 /** Creates a List of User's Courses
  * Courses -> CourseList -> CourseCard -> CourseInfo
  *
  * @param {array}	 courses	 Array of objects containing course data.
+ * @param {string}	 type		 String that is either 'current' or 'past'
  */
-function CourseList({ courses = [] }) {
-	const List = courses.map((course) => (
-		<>
-			<li key={course.id}>
-				<CourseCard
-					id={course.id}
-					department={course.data.course.abbreviation}
-					number={course.data.course.course_number}
-					term={course.data.semester}
-					title={course.data.course.title}
-				/>
-			</li>
-		</>
-	));
+function CourseList({ courses = [], type }) {
+	const List =
+		courses.length !== 0 ? (
+			courses.map((course) => (
+				<>
+					<li key={course.id}>
+						<CourseCard
+							id={course.id}
+							department={course.data.course.abbreviation}
+							number={course.data.course.course_number}
+							term={course.data.semester}
+							title={course.data.course.title}
+						/>
+					</li>
+				</>
+			))
+		) : (
+			<NoData text={'courses'} />
+		);
 
-	return <ul className="Course-List">{List}</ul>;
+	return (
+		<ul className="Course-List">
+			<li className="Course-List__Type">{capitalizeFirstLetter(type)}</li>
+			{List}
+		</ul>
+	);
 }
 
 CourseList.propTypes = {
 	courses: PropTypes.array,
+	type: PropTypes.string,
 };
 
 export default CourseList;
