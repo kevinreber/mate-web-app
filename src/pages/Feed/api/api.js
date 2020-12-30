642b8936e071ade7f3b8ac937b949066206f3bce
kevinreber <kevinreber1@gmail.com>
2020-12-30 10:30:52 -0800

    migrated fetch to axios

--- src/pages/Feed/api/api.js
+++ src/pages/Feed/api/api.js
@@ -1,20 +1,22 @@
+/** Dependencies */
+import axios from 'axios';
+
 /** Constants */
 import { FEED_URL, FEED_TYPES, BEARER_AUTH_TOKEN } from '../constants/index';
 
 export async function getFeed() {
 	// logs token we need to access API
 	console.log(BEARER_AUTH_TOKEN);
-	return fetch(FEED_URL, {
+	return axios(FEED_URL, {
 		method: 'GET',
 		headers: {
 			Authorization: BEARER_AUTH_TOKEN,
 			'Content-Type': 'application/json',
 		},
 	})
-		.then((response) => response.json())
 		.then((data) => {
 			console.log('Success:', data.data);
-			return data.data;
+			return data.data.data;
 		})
 		.catch((error) => {
 			console.error('Error:', error);
