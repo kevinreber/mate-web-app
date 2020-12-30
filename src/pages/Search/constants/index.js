b31602428687aa18984f26dd130bcd5153e53b55
kevinreber <kevinreber1@gmail.com>
2020-12-30 10:33:58 -0800

    added API constants

--- src/pages/Search/constants/index.js
+++ src/pages/Search/constants/index.js
@@ -7,3 +7,9 @@ export const SearchCategories = [
 	'Marketplace',
 	'Events',
 ];
+
+/** API Information */
+const BASE_URL = 'https://api.mateapp.us/api';
+export const FEED_URL = `${BASE_URL}/feeds`;
+export const FEED_TYPES = `${FEED_URL}/types`;
+export const BEARER_AUTH_TOKEN = localStorage.getItem('bearerAuthToken');
