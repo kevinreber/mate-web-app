6c383fbaf80d6ae474b041bb7076782932c3a3ed
kevinreber <kevinreber1@gmail.com>
2020-12-29 23:31:24 -0800

    moved API constants

--- src/pages/UserProfile/constants/index.js
+++ src/pages/UserProfile/constants/index.js
@@ -23,3 +23,9 @@ export const CONFIRM = {
 	title: 'Delete account?',
 	subtitle: "You can't undo this operation",
 };
+
+const BASE_URL = 'https://api.mateapp.us';
+
+export const USERS = `${BASE_URL}/users`;
+export const FOLLOW_USER = `${BASE_URL}/follow`;
+export const UNFOLLOW_USER = `${BASE_URL}/unfollow`;
