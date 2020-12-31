d914955fed462afcf0a6d2fc6f29e6a279673853
kevinreber <kevinreber1@gmail.com>
2020-12-31 00:10:22 -0800

    setup constants and callback to get all users data

---
+++ src/pages/Tutors/api/api.js
@@ -0,0 +1,20 @@
+/** Dependencies */
+import axios from 'axios';
+
+import { ALL_USERS, BEARER_AUTH_TOKEN } from '../api/api';
+
+export const getAllUsersData = () => {
+	return axios(ALL_USERS, {
+		method: 'GET',
+		headers: {
+			Authorization: 'Bearer ' + BEARER_AUTH_TOKEN,
+			'Content-Type': 'application/json',
+		},
+	})
+		.then((data) => {
+			return data;
+		})
+		.catch((error) => {
+			console.error('Error:', error);
+		});
+};
