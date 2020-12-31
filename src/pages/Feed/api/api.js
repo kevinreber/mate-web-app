679cabae5cbfe94de299b857875593ffad2eab25
kevinreber <kevinreber1@gmail.com>
2020-12-30 17:58:17 -0800

    wip: receiving errors when sending post requests for new posts

--- src/pages/Feed/api/api.js
+++ src/pages/Feed/api/api.js
@@ -23,23 +23,32 @@ export async function getFeed() {
 		});
 }
 
+//  TODO: Configure post data, receiving error message below when sending request
+// {"error":true,"message":"The given data was invalid.",
+// 	"data":{"
+// 		feed_type_id":["The feed type id field is required."],
+// 		"location":["The location field is required."]
+// }}
 export async function postFeed(data) {
-	return fetch(FEED_URL, {
-		method: 'POST',
-		headers: {
-			Authorization: BEARER_AUTH_TOKEN,
-			'Content-Type': 'application/json',
-		},
-		body: JSON.stringify(data),
-	})
-		.then((response) => response.json())
-		.then((data) => {
-			console.log('Success:', data.data);
-			return data.data;
+	console.log(data);
+	return (
+		axios(FEED_URL, {
+			method: 'POST',
+			headers: {
+				Authorization: BEARER_AUTH_TOKEN,
+				// 'Content-Type': 'application/json',
+			},
+			data,
 		})
-		.catch((error) => {
-			console.error('Error:', error);
-		});
+			// .then((response) => response.json())
+			.then((data) => {
+				console.log('Success:', data.data);
+				return data.data;
+			})
+			.catch((error) => {
+				console.error('Error:', error);
+			})
+	);
 }
 
 export async function getFeedTypes() {
